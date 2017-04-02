93b50b6d442445b460b2c8848defebe131d640c3
Debanjan Basu <debanjanbasu2006@gmail.com>
2017-04-02 23:14:57 +1000

    commit for updated build system

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,23 +1,14 @@
 'use strict';
-const webpack = require('webpack'),
-    UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
     target: 'node',
     output: {
         filename: '../dist/index.js',
-        libraryTarget: 'commonjs'
+        libraryTarget: 'commonjs2'
     },
     plugins: [
-        new UglifyJSPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            }
-        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
